a6f9c00f3ab173a8c5f5d3f8d9c45414768ee539
Rafał <rafalmiskiewicz9@gmail.com>
2020-03-26 17:01:12 +0100

    koniec

--- sob_lab_2/src/com/company/Main.java
+++ sob_lab_2/src/com/company/Main.java
@@ -6,44 +6,68 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
-        NumberSet set = new NumberSet();
-        Random generator = new Random();
-        assert (set != null);
-
-
         try {
-            for (int i = 0; i <= 5; i++)
-                set.add(generator.nextInt(5));
-            set.add(2);
-            set.print();
-            set.remove(2);
-            set.print();
-            set.remove(10);
-            set.print();
+            test9();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    static void test1() throws Exception {
+        NumberSet nSet = new NumberSet();
+        Random generator = new Random();
+            for (int i = 1; i <=100 ; i++)
+                nSet.add(generator.nextInt(5));
+            nSet.print();
+            nSet.add(2);
+    }
 
+    static void test2() throws Exception {
+        NumberSet nSet = new NumberSet();
+        Random generator = new Random();
+        nSet.maxSize=-5;
+            for (int i = 1; i <=100 ; i++)
+                nSet.add(generator.nextInt(5));
+            nSet.print();
+            nSet.add(2);
+    }
 
+    static void test3() throws Exception {
+        NumberSet nSet = new NumberSet();
+        nSet.add(5);
+    }
 
+    static void test4() throws Exception {
+        NumberSet nSet = new NumberSet();
+        Random generator = new Random();
+        for (int i = 1; i <=10 ; i++)
+            nSet.add(generator.nextInt(5));
+        nSet.maxSize=5;
+    }
 
-//            set.remove(10);
-//            System.out.println();
-//            System.out.println();
-//            set.print();
-//            set.getRandomValue();
-//            set.print();
-//            set.getSumPfElements();
-//            set.print();
-//            set.divideAllElementsBy(2);
-//            set.print();
-//            set.contains(2);
+    static void test5() throws Exception {
+        NumberSet nSet = new NumberSet();
+        System.out.println(nSet.getRandomValue());
+    }
 
+    static void test6() throws Exception {
+        NumberSet nSet = new NumberSet();
+        System.out.println(nSet.getSumOfElements());
+    }
 
+    static void test7() throws Exception {
+        NumberSet nSet = new NumberSet();
+        nSet.divideAllElementsBy(0);
+    }
 
+    static void test8() throws Exception {
+        NumberSet nSet = new NumberSet();
+        nSet.contains(0);
+    }
 
+    static void test9() throws Exception {
+        NumberSet nSet = new NumberSet();
+        nSet.getSize();
     }
 }
 
--- sob_lab_2/src/com/company/NumberSet.java
+++ sob_lab_2/src/com/company/NumberSet.java
@@ -8,13 +8,32 @@ public class NumberSet {
     private int[] nSet = new int[maxSize];
     private  int size=0;
 
+    /** Metoda dodaje liczbę do zbioru liczb (zezwalamy na dodanie just istniejącej liczby)
+     *  @param i - liczba, którą dodajemy
+     *  @throws Exception - występuje w przypadku przepełnienia zbioru
+     */
     public void add(int i) throws Exception{
-        assert (size<maxSize): "Overflow table";
-        nSet[size]=i;
-        size+=1;
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (Integer.class.isInstance(i)) : "Błędny typ zmiennej i";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        if(size>=maxSize)
+            throw new Exception("Wystąpiło przepełnienie zbioru przy próbie dodania!");
+        else {
+            nSet[size] = i;
+            size += 1;
+        }
     }
 
+    /** Metoda usuwa liczbę ze zbioru (każde wystąpienie)
+     *  @param liczba - liczba do usunięcia
+     *  @throws Exception - występuje, jeśli zbiór nie posiada liczby którą chcemy usunąć
+     */
     public void remove(int liczba) throws Exception{
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (Integer.class.isInstance(liczba)) : "Błędny typ zmiennej liczba";
+        assert (this.size != 0) : "Zbiór pusty!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
         int[] temp = new int[maxSize];
         int j=0;
         boolean flaga;
@@ -24,13 +43,21 @@ public class NumberSet {
                 j++;
             }
         }
-        assert (j!=size):"no number found";
+        if (j==size)
+            throw new Exception("nie znaleziono liczby do usunięcia w zbiorze");
         nSet=temp;
         size=nSet.length;
     }
 
+    /** Metoda losuje jedną liczbę ze zbioru oraz ją usuwa
+     *  @return - wylosowana liczba
+     *  @throws Exception - występuje, jeśli zbiór jest pusty
+     */
     public int getRandomValue() throws Exception{
-        assert(size<=0);
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        if(this.size == 0)  throw new Exception("Zbiór pusty!");
         Random generator = new Random();
         int random = generator.nextInt(size);
         int number=nSet[random];
@@ -39,7 +66,17 @@ public class NumberSet {
         return number;
     }
 
+//--------------------------------
+
+    /** Metoda zwraca sumę podanych elementów d
+     *  @return - suma liczb
+     *  @throws Exception - występuje, jeśli zbiór jest pusty
+     */
     public int getSumOfElements() throws Exception{
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        if(this.size == 0)  throw new Exception("Zbiór pusty!");
         assert(size<=0);
         int suma=0;
         for (int liczba:nSet)
@@ -47,13 +84,33 @@ public class NumberSet {
         return suma;
     }
 
+    /** Metoda dzieli każdy element zbioru przez podaną wartość bez reszty
+     *   @param d - liczba, przez którą dzielimy
+     *   @throws Exception - występuje w przypadku dzielenia przez 0 (można zastąpić asercją)
+     */
     public void divideAllElementsBy(int d) throws Exception{
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        assert (this.size != 0) : "Zbiór pusty!";
+        assert(size<=0);
         assert (d!=0);
+
         for(int i=0;i<size;i++)
             nSet[i]=nSet[i]/d;
     }
 
+    /** Metoda sprawdza, czy w zbiorze istnieje podany element
+     *   @param i - element do sprawdzenia
+     *   @return true w przypadku odnalezienia wartości, false w przeciwnym razie
+     */
     public boolean contains (int i){
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        assert (this.size != 0) : "Zbiór pusty!";
+        assert(size<=0);
+
         for (int liczba:nSet) {
             if(liczba==i)
                 return true;
@@ -61,7 +118,15 @@ public class NumberSet {
         return false;
     }
 
+    /** Metoda zwraca rozmiar zbioru (liczbę elementów)
+     *  @return - rozmiar zbioru
+     */
     public int getSize(){
+        assert (this.size < maxSize) : "Wystąpiło przepełnienie zbioru!";
+        assert (this.nSet != null) : "Zbiór nie został utworzony!";
+        assert (maxSize>0) : "Maksymalna liczba elementów mniejsza niż 0";
+        assert (this.size != 0) : "Zbiór pusty!";
+        assert(size<=0);
         return size;
     }
 
@@ -69,7 +134,8 @@ public class NumberSet {
     public String toString() {
         return nSet.toString();
     }
-
+    /** Pozwala sprawdzić wartości w tablicy
+     */
     public void print(){
         for (int i = 0; i < size; i++){
             System.out.print(i + ":" + nSet[i]+", ");
